43b1d0fc7f3fc89359be0e85b782dd855ced1487
John <lichunxiao0404>
2020-06-05 19:22:41 +0800

    default configuration

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+'''
+
+Dafault Configuration
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'Bbeida45',
+        'db': 'awesome' 
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
